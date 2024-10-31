6001037b3bde5d4be1c8a2b451d70190714ccc93
Yondon Fu <yondon.fu@gmail.com>
2024-10-31 01:01:52 +0000

    Add install script for node req

---
+++ install.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import argparse
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def install_custom_node_req(workspace: str):
+    custom_nodes_path = os.path.join(workspace, "custom_nodes")
+    for folder in os.listdir(custom_nodes_path):
+        folder_path = os.path.join(custom_nodes_path, folder)
+        req_file = os.path.join(folder_path, "requirements.txt")
+
+        if os.path.isdir(folder_path) and os.path.isfile(req_file):
+            logger.info(f"Installing requirements for {folder}...")
+            subprocess.run(["pip", "install", "-r", req_file], check=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Install custom node requirements")
+    parser.add_argument(
+        "--workspace", default=None, required=True, help="Set Comfy workspace"
+    )
+    args = parser.parse_args()
+
+    install_custom_node_req(args.workspace)
