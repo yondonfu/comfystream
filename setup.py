59e6bb78d519469b1cab2dd976ff578ff42cd8df
Yondon Fu <yondon.fu@gmail.com>
2024-10-31 01:02:13 +0000

    Pin hiddenswitch/comfyui version
    89d07f3adf32a6703181343bc732bd85104bb653

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         "asyncio",
-        "comfyui @ git+https://github.com/hiddenswitch/ComfyUI.git",
+        "comfyui @ git+https://github.com/hiddenswitch/ComfyUI.git@89d07f3adf32a6703181343bc732bd85104bb653",
     ],
     url="https://github.com/yondonfu/comfystream",
 )
