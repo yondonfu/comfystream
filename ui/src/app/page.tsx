b5b3adfaaf1575539968d45d980aef6ac694e18d
ryanontheinstide <ryanfosdick87@gmail.com>
2024-12-04 15:58:31 -0500

    control panel

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,5 +1,11 @@
 import { Room } from "@/components/room";
+import { ControlPanel } from "@/components/control-panel";
 
 export default async function Page() {
-  return <Room />;
+  return (
+    <>
+      <Room />
+      <ControlPanel />
+    </>
+  );
 }
--- ui/src/components/control-panel.tsx
+++ ui/src/components/control-panel.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { usePeerContext } from "@/context/peer-context";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
