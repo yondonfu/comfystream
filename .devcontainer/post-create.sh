233e74a9f9ea95e98c0757d0196e6e9017cdfcea
Elite <john@eliteencoder.net>
2025-01-24 01:41:28 +0000

    add model download, add python extensions for managing conda shell integration

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Create a symlink to the extra_model_paths.yaml file only if /ComfyUI/extra/models exists and /ComfyUI/extra_model_paths.yaml does not exist, otherwise remove the link
-if [ -d "/ComfyUI/extra/models" ] && [ ! -L "/ComfyUI/extra_model_paths.yaml" ]; then
-    ln -s /workspace/.devcontainer/extra_model_paths.yaml /ComfyUI/extra_model_paths.yaml
-else
-    if [ -L "/ComfyUI/extra_model_paths.yaml" ]; then
-        rm /ComfyUI/extra_model_paths.yaml
-    fi
-fi
-
 # Initialize conda if needed
 if ! command -v conda &> /dev/null; then
     /miniconda3/bin/conda init bash
