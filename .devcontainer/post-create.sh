0c5a03414b4f1849f6e9953a4ed79395d7930447
Elite Encoder <john@eliteencoder.net>
2025-01-29 16:33:52 -0500

    move symbolic link to post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,9 +5,15 @@ if ! command -v conda &> /dev/null; then
     /miniconda3/bin/conda init bash
 fi
 
+# Install npm packages if needed
 cd /workspace/ui
 if [ ! -d "node_modules" ]; then
     npm install --legacy-peer-deps   
+fi
+
+# Create a symlink to the ComfyUI workspace
+if [ ! -d "/workspace/ComfyUI" ]; then
+    ln -s /ComfyUI /workspace/ComfyUI
 fi
 
 cd /workspace
