ea611fcd08ea143df268aa1e91c4cff4517c6b99
Rick Staa <rick.staa@outlook.com>
2025-02-06 08:52:13 +0100

    feat: add JSON and video error toasters
    This commit ensures that users are aware if there has been an error trying
    to get their camara devices or parsing of the workflow json.

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
