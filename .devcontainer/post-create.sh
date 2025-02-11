9d18bcf728ca6c03271df8a7debd01aa440240fb
Elite <john@eliteencoder.net>
2025-02-11 00:48:19 +0000

    fix more workspace paths

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# Install npm packages if needed
-cd /workspace/comfystream/ui
-if [ ! -d "node_modules" ]; then
-    npm install --legacy-peer-deps   
-fi
-
-# Create a symlink to the ComfyUI workspace
-if [ ! -d "/workspace/comfystream/ComfyUI" ]; then
-    ln -s /workspace/ComfyUI /workspace/comfystream/ComfyUI
-fi
-
 cd /workspace/comfystream
 /bin/bash
