315663c29c208aa12c693ba296d082b3f7421fc4
Brad P <brad@ad-astra.video>
2025-02-13 21:32:56 +0000

    reload embedded client

--- server/api/nodes/nodes.py
+++ server/api/nodes/nodes.py
@@ -127,3 +127,59 @@ async def delete_node(node, workspace_dir):
     except Exception as e:
         logger.error(f"error deleting node {node['name']}")
         raise Exception(f"error deleting node: {e}")
+
+
+from comfy.nodes.package import ExportedNodes
+from comfy.nodes.package import _comfy_nodes, _import_and_enumerate_nodes_in_module
+from functools import reduce
+from importlib.metadata import entry_points
+import types
+
+def force_import_all_nodes_in_workspace(vanilla_custom_nodes=True, raise_on_failure=False) -> ExportedNodes:
+    # now actually import the nodes, to improve control of node loading order
+    from comfy_extras import nodes as comfy_extras_nodes  # pylint: disable=absolute-import-used
+    from comfy.cli_args import args
+    from comfy.nodes import base_nodes
+    from comfy.nodes.vanilla_node_importing import mitigated_import_of_vanilla_custom_nodes
+
+    # only load these nodes once
+
+    base_and_extra = reduce(lambda x, y: x.update(y),
+                            map(lambda module_inner: _import_and_enumerate_nodes_in_module(module_inner, raise_on_failure=raise_on_failure), [
+                                # this is the list of default nodes to import
+                                base_nodes,
+                                comfy_extras_nodes
+                            ]),
+                            ExportedNodes())
+    custom_nodes_mappings = ExportedNodes()
+
+    if args.disable_all_custom_nodes:
+        logging.info("Loading custom nodes was disabled, only base and extra nodes were loaded")
+        _comfy_nodes.update(base_and_extra)
+        return _comfy_nodes
+
+    # load from entrypoints
+    for entry_point in entry_points().select(group='comfyui.custom_nodes'):
+        # Load the module associated with the current entry point
+        try:
+            module = entry_point.load()
+        except ModuleNotFoundError as module_not_found_error:
+            logging.error(f"A module was not found while importing nodes via an entry point: {entry_point}. Please ensure the entry point in setup.py is named correctly", exc_info=module_not_found_error)
+            continue
+
+        # Ensure that what we've loaded is indeed a module
+        if isinstance(module, types.ModuleType):
+            custom_nodes_mappings.update(
+                _import_and_enumerate_nodes_in_module(module, print_import_times=True))
+
+    # load the vanilla custom nodes last
+    if vanilla_custom_nodes:
+        custom_nodes_mappings += mitigated_import_of_vanilla_custom_nodes()
+
+    # don't allow custom nodes to overwrite base nodes
+    custom_nodes_mappings -= base_and_extra
+
+    _comfy_nodes.update(base_and_extra + custom_nodes_mappings)
+    
+    return _comfy_nodes
+
