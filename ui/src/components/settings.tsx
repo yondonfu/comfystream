yondonfu/comfystream

commit 9045ade6582c5e678346cc070a32b7c61cb64fa4

feat: audio support  (#10)
* Squashed commit of the following:

commit a685e111c1e0934cd741f221c36489877055ae99
Author: Varshith B <varshith15@gmail.com>
Date:   Mon Feb 17 22:46:25 2025 +0530

    feat: add cleanup

commit 0e4d8afb4e2ed2f9eecd8adfeea8a562eee5e3f4
Author: Varshith B <varshith15@gmail.com>
Date:   Sun Feb 16 22:52:46 2025 +0530

    fix: audio frame skipping

commit 44df1701cfbbf44a3095ad65fd1802048ccce6a6
Author: Varshith B <varshith15@gmail.com>
Date:   Sun Feb 16 22:18:23 2025 +0530

    fix: frame skipping

commit c3009c69d7102e4cdd0d5817600566609daea200
Author: Varshith B <varshith15@gmail.com>
Date:   Thu Feb 13 18:49:26 2025 +0530

    fix: dynamic prompt change

commit 383105b4d16e8df84da2bd9fbe03abaa2ac044a6
Author: Elite Encoder <john@eliteencoder.net>
Date:   Wed Feb 12 21:24:34 2025 +0000

    fix prompt control panel rendering (wip)

commit b372b10a672b64ef3d378c5dece13d7aa1cc4795
Author: Elite Encoder <john@eliteencoder.net>
Date:   Wed Feb 12 18:11:50 2025 +0000

    add libsndfile1

commit 214cfec938ff774e78218eebe7e9a98f596be664
Merge: a0ed6f8 6f54863
Author: Varshith Bathini <varshith15@gmail.com>
Date:   Tue Feb 11 22:43:18 2025 +0530

    Merge branch 'main' into feat/audio_support

commit a0ed6f8b4b3918c770039c7b4d5ee8a9ba292326
Author: Varshith Bathini <varshith15@gmail.com>
Date:   Fri Feb 7 01:54:42 2025 +0530

    Apply suggestions from code review

    Co-authored-by: John | Elite Encoder <john@eliteencoder.net>

commit b85d01d5e39b9e9cbb65b0cc6eaaab391e373314
Author: Varshith B <varshith15@gmail.com>
Date:   Tue Feb 4 17:29:17 2025 +0530

    fix: update prompts

commit aa209f00b36bd7bde643c5855ccaa8d16e5d0122
Merge: 6e134b7 af132be
Author: Varshith B <varshith15@gmail.com>
Date:   Mon Feb 3 23:01:48 2025 +0530

    Merge branch 'feat/audio_support' of https://github.com/varshith15/comfystream into feat/audio_support

ui/src/components/settings.tsx
@@ -40,8 +40,9 @@ import { Select } from "./ui/select";
 export interface StreamConfig {
   streamUrl: string;
   frameRate: number;
-  prompt?: any;
+  prompts?: any;
   selectedDeviceId: string | undefined;
+  selectedAudioDeviceId: string | undefined;
 }
 
 interface VideoDevice {
@@ -54,6 +55,7 @@ export const DEFAULT_CONFIG: StreamConfig = {
     process.env.NEXT_PUBLIC_DEFAULT_STREAM_URL || "http://127.0.0.1:8889",
   frameRate: 30,
   selectedDeviceId: undefined,
+  selectedAudioDeviceId: undefined,
 };
 
 interface StreamSettingsProps {
@@ -117,28 +119,28 @@ interface ConfigFormProps {
 }
 
 interface PromptContextType {
-  originalPrompt: any;
-  currentPrompt: any;
-  setOriginalPrompt: (prompt: any) => void;
-  setCurrentPrompt: (prompt: any) => void;
+  originalPrompts: any;
+  currentPrompts: any;
+  setOriginalPrompts: (prompts: any) => void;
+  setCurrentPrompts: (prompts: any) => void;
 }
 
 export const PromptContext = createContext<PromptContextType>({
-  originalPrompt: null,
-  currentPrompt: null,
-  setOriginalPrompt: () => {},
-  setCurrentPrompt: () => {},
+  originalPrompts: null,
+  currentPrompts: null,
+  setOriginalPrompts: () => {},
+  setCurrentPrompts: () => {},
 });
 
 export const usePrompt = () => useContext(PromptContext);
 
 function ConfigForm({ config, onSubmit }: ConfigFormProps) {
-  const [prompt, setPrompt] = useState<any>(null);
-  const { setOriginalPrompt } = usePrompt();
+  const [prompts, setPrompts] = useState<any[]>([]);
+  const { setOriginalPrompts } = usePrompt();
   const [videoDevices, setVideoDevices] = useState<VideoDevice[]>([]);
-  const [selectedDevice, setSelectedDevice] = useState<string | undefined>(
-    config.selectedDeviceId
-  );
+  const [audioDevices, setAudioDevices] = useState<VideoDevice[]>([]);
+  const [selectedDevice, setSelectedDevice] = useState<string | undefined>(config.selectedDeviceId);
+  const [selectedAudioDevice, setSelectedAudioDevice] = useState<string | undefined>(config.selectedDeviceId);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -150,38 +152,78 @@ function ConfigForm({ config, onSubmit }: ConfigFormProps) {
    */
   const getVideoDevices = useCallback(async () => {
     try {
-      // Get Available Video Devices.
-      await navigator.mediaDevices.getUserMedia({ video: true });
-      const devices = await navigator.mediaDevices.enumerateDevices();
-      const videoDevices = devices
-        .filter((device) => device.kind === "videoinput")
-        .map((device) => ({
-          deviceId: device.deviceId,
-          label: device.label || `Camera ${device.deviceId.slice(0, 5)}...`,
-        }));
-      setVideoDevices(videoDevices);
+      await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
 
-      // Use first device as default and remove selected device if unavailable.
-      if (!videoDevices.some((device) => device.deviceId === selectedDevice)) {
-        setSelectedDevice(videoDevices.length > 0 ? videoDevices[0].deviceId : undefined);
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = [
+        { deviceId: "none", label: "No Video" },
+        ...devices
+          .filter((device) => device.kind === "videoinput")
+          .map((device) => ({
+            deviceId: device.deviceId,
+            label: device.label || `Camera ${device.deviceId.slice(0, 5)}...`,
+          }))
+      ];
+
+      setVideoDevices(videoDevices);
+      // Set default to first available camera if no selection yet
+      if (!selectedDevice && videoDevices.length > 1) {
+        setSelectedDevice(videoDevices[1].deviceId); // Index 1 because 0 is "No Video"
       }
-    } catch (err){
-      console.log(`Failed to get video devices: ${err}`);
+    } catch (err) {
+      console.error("Failed to get video devices");
+      // If we can't access video devices, still provide the None option
+      const videoDevices = [{ deviceId: "none", label: "No Video" }];
+      setVideoDevices(videoDevices);
+      setSelectedDevice("none");
     }
   }, [selectedDevice]);
+
+  const getAudioDevices = useCallback(async () => {
+    try {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const audioDevices = [
+        { deviceId: "none", label: "No Audio" },
+        ...devices
+          .filter((device) => device.kind === "audioinput")
+          .map((device) => ({
+            deviceId: device.deviceId,
+            label: device.label || `Microphone ${device.deviceId.slice(0, 5)}...`,
+          }))
+      ];
+
+      setAudioDevices(audioDevices);
+      // Set default to first available microphone if no selection yet
+      if (!selectedAudioDevice && audioDevices.length > 1) {
+        setSelectedAudioDevice(audioDevices[0].deviceId); // Default to "No Audio" for now
+      }
+    } catch (err) {
+      console.error("Failed to get audio devices");
+      // If we can't access audio devices, still provide the None option
+      const audioDevices = [{ deviceId: "none", label: "No Audio" }];
+      setAudioDevices(audioDevices);
+      setSelectedAudioDevice("none");
+    }
+  }, [selectedAudioDevice]);
 
   // Handle device change events.
   useEffect(() => {
     getVideoDevices();
+    getAudioDevices();
     navigator.mediaDevices.addEventListener("devicechange", getVideoDevices);
+    navigator.mediaDevices.addEventListener("devicechange", getAudioDevices);
 
     return () => {
       navigator.mediaDevices.removeEventListener(
         "devicechange",
         getVideoDevices
       );
+      navigator.mediaDevices.removeEventListener(
+        "devicechange",
+        getAudioDevices
+      );
     };
-  }, [getVideoDevices]);
+  }, [getVideoDevices, getAudioDevices]);
 
   const handleSubmit = (values: z.infer<typeof formSchema>) => {
     onSubmit({
@@ -189,22 +231,27 @@ function ConfigForm({ config, onSubmit }: ConfigFormProps) {
       streamUrl: values.streamUrl
         ? values.streamUrl.replace(/\/+$/, "")
         : values.streamUrl,
-      prompt,
+      prompts: prompts,
       selectedDeviceId: selectedDevice,
+      selectedAudioDeviceId: selectedAudioDevice,
     });
   };
 
-  const handlePromptChange = async (e: any) => {
-    const file = e.target.files[0];
-    if (!file) return;
+  const handlePromptsChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files?.length) return;
 
     try {
-      const text = await file.text();
-      const parsedPrompt = JSON.parse(text);
-      setPrompt(parsedPrompt);
-      setOriginalPrompt(parsedPrompt);
+      const files = Array.from(e.target.files);
+      const fileReads = files.map(async (file) => {
+        const text = await file.text();
+        return JSON.parse(text);
+      });
+
+      const allPrompts = await Promise.all(fileReads);
+      setPrompts(allPrompts);
+      setOriginalPrompts(allPrompts);
     } catch (err) {
-      console.error(err);
+      console.error("Failed to parse one or more JSON files.", err);
     }
   };
 
@@ -257,10 +304,7 @@ function ConfigForm({ config, onSubmit }: ConfigFormProps) {
             onValueChange={handleCameraSelect}
           >
             <Select.Trigger className="w-full mt-2">
-              {videoDevices.length === 0
-                ? "No camera devices found"
-                : videoDevices.find((d) => d.deviceId === selectedDevice)
-                    ?.label || "Select camera"}
+              {selectedDevice ? (videoDevices.find((d) => d.deviceId === selectedDevice)?.label || "None") : "None"}
             </Select.Trigger>
             <Select.Content>
               {videoDevices.length === 0 ? (
@@ -278,14 +322,30 @@ function ConfigForm({ config, onSubmit }: ConfigFormProps) {
           </Select>
         </div>
 
+        <div className="mt-4 mb-4">
+          <Label>Microphone</Label>
+          <Select value={selectedAudioDevice} onValueChange={setSelectedAudioDevice}>
+            <Select.Trigger className="w-full mt-2">
+              {selectedAudioDevice ? (audioDevices.find((d) => d.deviceId === selectedAudioDevice)?.label || "None") : "None"}
+            </Select.Trigger>
+            <Select.Content>
+              {audioDevices.map((device) => (
+                <Select.Option key={device.deviceId} value={device.deviceId}>
+                  {device.label}
+                </Select.Option>
+              ))}
+            </Select.Content>
+          </Select>
+        </div>
+
         <div className="mt-4 mb-4 grid max-w-sm items-center gap-3">
-          <Label>Comfy Workflow</Label>
+          <Label>Comfy Workflows</Label>
           <Input
-            id="workflow"
+            id="video-workflow"
             type="file"
             accept=".json"
-            onChange={handlePromptChange}
-            required={true}
+            multiple
+            onChange={handlePromptsChange}
           />
         </div>
 

docker/Dockerfile.base
@@ -9,6 +9,7 @@ RUN export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install -
     wget \
     nano \
     socat \
+    libsndfile1 \
     build-essential llvm tk-dev \
     && rm -rf /var/lib/apt/lists/*
 

nodes/audio_utils/__init__.py
@@ -1,0 +1,7 @@
+from .load_audio_tensor import LoadAudioTensor
+from .save_audio_tensor import SaveAudioTensor
+from .pitch_shift import PitchShifter
+
+NODE_CLASS_MAPPINGS = {"LoadAudioTensor": LoadAudioTensor, "SaveAudioTensor": SaveAudioTensor, "PitchShifter": PitchShifter}
+
+__all__ = ["NODE_CLASS_MAPPINGS"]

nodes/audio_utils/load_audio_tensor.py
@@ -1,0 +1,52 @@
+import numpy as np
+
+from comfystream import tensor_cache
+
+class LoadAudioTensor:
+    CATEGORY = "audio_utils"
+    RETURN_TYPES = ("WAVEFORM", "INT")
+    FUNCTION = "execute"
+    
+    def __init__(self):
+        self.audio_buffer = np.empty(0, dtype=np.int16)
+        self.buffer_samples = None
+        self.sample_rate = None
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "buffer_size": ("FLOAT", {"default": 500.0}),
+            }
+        }
+    
+    @classmethod
+    def IS_CHANGED():
+        return float("nan")
+    
+    def execute(self, buffer_size):
+        if self.sample_rate is None or self.buffer_samples is None:
+            frame = tensor_cache.audio_inputs.get(block=True)
+            self.sample_rate = frame.sample_rate
+            self.buffer_samples = int(self.sample_rate * buffer_size / 1000)
+            self.leftover = frame.side_data.input
+        
+        if self.leftover.shape[0] < self.buffer_samples:
+            chunks = [self.leftover] if self.leftover.size > 0 else []
+            total_samples = self.leftover.shape[0]
+            
+            while total_samples < self.buffer_samples:
+                frame = tensor_cache.audio_inputs.get(block=True)
+                if frame.sample_rate != self.sample_rate:
+                    raise ValueError("Sample rate mismatch")
+                chunks.append(frame.side_data.input)
+                total_samples += frame.side_data.input.shape[0]
+            
+            merged_audio = np.concatenate(chunks, dtype=np.int16)
+            buffered_audio = merged_audio[:self.buffer_samples]
+            self.leftover = merged_audio[self.buffer_samples:]
+        else:
+            buffered_audio = self.leftover[:self.buffer_samples]
+            self.leftover = self.leftover[self.buffer_samples:]
+                
+        return buffered_audio, self.sample_rate

nodes/audio_utils/pitch_shift.py
@@ -1,0 +1,32 @@
+import numpy as np
+import librosa
+
+class PitchShifter:
+    CATEGORY = "audio_utils"
+    RETURN_TYPES = ("WAVEFORM", "INT")
+    FUNCTION = "execute"
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "audio": ("WAVEFORM",),
+                "sample_rate": ("INT",),
+                "pitch_shift": ("FLOAT", {
+                    "default": 4.0, 
+                    "min": 0.0, 
+                    "max": 12.0, 
+                    "step": 0.5
+                }),
+            }
+        }
+
+    @classmethod
+    def IS_CHANGED(cls):
+        return float("nan")
+
+    def execute(self, audio, sample_rate, pitch_shift):
+        audio_float = audio.astype(np.float32) / 32768.0
+        shifted_audio = librosa.effects.pitch_shift(y=audio_float, sr=sample_rate, n_steps=pitch_shift)
+        shifted_int16 = np.clip(shifted_audio * 32768.0, -32768, 32767).astype(np.int16)
+        return shifted_int16, sample_rate

nodes/audio_utils/save_audio_tensor.py
@@ -1,19 +1,17 @@
-import torch
-
 from comfystream import tensor_cache
 
-
-class SaveTensor:
-    CATEGORY = "tensor_utils"
+class SaveAudioTensor:
+    CATEGORY = "audio_utils"
     RETURN_TYPES = ()
     FUNCTION = "execute"
     OUTPUT_NODE = True
+
 
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "images": ("IMAGE",),
+                "audio": ("WAVEFORM",)
             }
         }
 
@@ -21,6 +19,7 @@ class SaveTensor:
     def IS_CHANGED(s):
         return float("nan")
 
-    def execute(self, images: torch.Tensor):
-        tensor_cache.image_outputs.put_nowait(images)
-        return images
+    def execute(self, audio):
+        tensor_cache.audio_outputs.put_nowait(audio)
+        return (audio,)
+

nodes/tensor_utils/load_tensor.py
@@ -15,5 +15,6 @@ class LoadTensor:
         return float("nan")
 
     def execute(self):
-        input = tensor_cache.inputs.pop()
-        return (input,)
+        frame = tensor_cache.image_inputs.get(block=True)
+        frame.side_data.skipped = False
+        return (frame.side_data.input,)

server/app.py
@@ -30,15 +30,44 @@ MIN_BITRATE = 2000000
 
 class VideoStreamTrack(MediaStreamTrack):
     kind = "video"
-
     def __init__(self, track: MediaStreamTrack, pipeline):
         super().__init__()
         self.track = track
         self.pipeline = pipeline
+        asyncio.create_task(self.collect_frames())
+
+    async def collect_frames(self):
+        while True:
+            try:
+                frame = await self.track.recv()
+                await self.pipeline.put_video_frame(frame)
+            except Exception as e:
+                await self.pipeline.cleanup()
+                raise Exception(f"Error collecting video frames: {str(e)}")
 
     async def recv(self):
-        frame = await self.track.recv()
-        return await self.pipeline(frame)
+        return await self.pipeline.get_processed_video_frame()
+    
+
+class AudioStreamTrack(MediaStreamTrack):
+    kind = "audio"
+    def __init__(self, track: MediaStreamTrack, pipeline):
+        super().__init__()
+        self.track = track
+        self.pipeline = pipeline
+        asyncio.create_task(self.collect_frames())
+
+    async def collect_frames(self):
+        while True:
+            try:
+                frame = await self.track.recv()
+                await self.pipeline.put_audio_frame(frame)
+            except Exception as e:
+                await self.pipeline.cleanup()
+                raise Exception(f"Error collecting audio frames: {str(e)}")
+
+    async def recv(self):
+        return await self.pipeline.get_processed_audio_frame()
 
 
 def force_codec(pc, sender, forced_codec):
@@ -87,8 +116,7 @@ async def offer(request):
 
     params = await request.json()
 
-    pipeline.set_prompt(params["prompt"])
-    await pipeline.warm()
+    await pipeline.set_prompts(params["prompts"])
 
     offer_params = params["offer"]
     offer = RTCSessionDescription(sdp=offer_params["sdp"], type=offer_params["type"])
@@ -103,17 +131,19 @@ async def offer(request):
 
     pcs.add(pc)
 
-    tracks = {"video": None}
+    tracks = {"video": None, "audio": None}
 
-    # Prefer h264
-    transceiver = pc.addTransceiver("video")
-    caps = RTCRtpSender.getCapabilities("video")
-    prefs = list(filter(lambda x: x.name == "H264", caps.codecs))
-    transceiver.setCodecPreferences(prefs)
+    # Only add video transceiver if video is present in the offer
+    if "m=video" in offer.sdp:
+        # Prefer h264
+        transceiver = pc.addTransceiver("video")
+        caps = RTCRtpSender.getCapabilities("video")
+        prefs = list(filter(lambda x: x.name == "H264", caps.codecs))
+        transceiver.setCodecPreferences(prefs)
 
-    # Monkey patch max and min bitrate to ensure constant bitrate
-    h264.MAX_BITRATE = MAX_BITRATE
-    h264.MIN_BITRATE = MIN_BITRATE
+        # Monkey patch max and min bitrate to ensure constant bitrate
+        h264.MAX_BITRATE = MAX_BITRATE
+        h264.MIN_BITRATE = MIN_BITRATE
 
     # Handle control channel from client
     @pc.on("datachannel")
@@ -131,13 +161,13 @@ async def offer(request):
                             "nodes": nodes_info
                         }
                         channel.send(json.dumps(response))
-                    elif params.get("type") == "update_prompt":
-                        if "prompt" not in params:
+                    elif params.get("type") == "update_prompts":
+                        if "prompts" not in params:
                             logger.warning("[Control] Missing prompt in update_prompt message")
                             return
-                        pipeline.set_prompt(params["prompt"])
+                        await pipeline.update_prompts(params["prompts"])
                         response = {
-                            "type": "prompt_updated",
+                            "type": "prompts_updated",
                             "success": True
                         }
                         channel.send(json.dumps(response))
@@ -158,6 +188,10 @@ async def offer(request):
 
             codec = "video/H264"
             force_codec(pc, sender, codec)
+        elif track.kind == "audio":
+            audioTrack = AudioStreamTrack(track, pipeline)
+            tracks["audio"] = audioTrack
+            pc.addTrack(audioTrack)
 
         @track.on("ended")
         async def on_ended():
@@ -175,6 +209,11 @@ async def offer(request):
 
     await pc.setRemoteDescription(offer)
 
+    if "m=audio" in pc.remoteDescription.sdp:
+        await pipeline.warm_audio()
+    if "m=video" in pc.remoteDescription.sdp:
+        await pipeline.warm_video()
+
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
 
@@ -190,7 +229,7 @@ async def set_prompt(request):
     pipeline = request.app["pipeline"]
 
     prompt = await request.json()
-    pipeline.set_prompt(prompt)
+    await pipeline.set_prompts(prompt)
 
     return web.Response(content_type="application/json", text="OK")
 

commit 50e6aff050d848d08b625394a11c171b07564d7e

fix(ui): ensure video error handling is working
This commit fixes some UI bugs that were introduced in #10, which caused the
app to throw an error on video device discovery.

ui/src/components/settings.tsx
@@ -158,50 +158,54 @@ function ConfigForm({ config, onSubmit }: ConfigFormProps) {
       const videoDevices = [
         { deviceId: "none", label: "No Video" },
         ...devices
-          .filter((device) => device.kind === "videoinput")
+          .filter((device) => device.kind === "videoinput" && device.deviceId)
           .map((device) => ({
             deviceId: device.deviceId,
             label: device.label || `Camera ${device.deviceId.slice(0, 5)}...`,
           }))
       ];
-
       setVideoDevices(videoDevices);
+
       // Set default to first available camera if no selection yet
       if (!selectedDevice && videoDevices.length > 1) {
         setSelectedDevice(videoDevices[1].deviceId); // Index 1 because 0 is "No Video"
       }
-    } catch (err) {
-      console.error("Failed to get video devices");
+    } catch (error) {
+      console.info("Failed to get video devices: ", error);
       // If we can't access video devices, still provide the None option
-      const videoDevices = [{ deviceId: "none", label: "No Video" }];
-      setVideoDevices(videoDevices);
+      setVideoDevices([{ deviceId: "none", label: "No Video" }]);
       setSelectedDevice("none");
     }
   }, [selectedDevice]);
 
+  /**
+   * Retrieves the list of audio devices available on the user's device.
+   */
   const getAudioDevices = useCallback(async () => {
     try {
       const devices = await navigator.mediaDevices.enumerateDevices();
       const audioDevices = [
         { deviceId: "none", label: "No Audio" },
         ...devices
-          .filter((device) => device.kind === "audioinput")
+          .filter((device) => device.kind === "audioinput" && device.deviceId)
           .map((device) => ({
             deviceId: device.deviceId,
             label: device.label || `Microphone ${device.deviceId.slice(0, 5)}...`,
           }))
       ];
-
       setAudioDevices(audioDevices);
+
       // Set default to first available microphone if no selection yet
       if (!selectedAudioDevice && audioDevices.length > 1) {
         setSelectedAudioDevice(audioDevices[0].deviceId); // Default to "No Audio" for now
+      } else {
+        setAudioDevices([{ deviceId: "none", label: "No Audio" }]);
+        setSelectedAudioDevice("none");
       }
-    } catch (err) {
-      console.error("Failed to get audio devices");
+    } catch (error) {
+      console.error("Failed to get audio devices: ", error);
       // If we can't access audio devices, still provide the None option
-      const audioDevices = [{ deviceId: "none", label: "No Audio" }];
-      setAudioDevices(audioDevices);
+      setAudioDevices([{ deviceId: "none", label: "No Audio" }]);
       setSelectedAudioDevice("none");
     }
   }, [selectedAudioDevice]);
@@ -346,6 +350,7 @@ function ConfigForm({ config, onSubmit }: ConfigFormProps) {
             accept=".json"
             multiple
             onChange={handlePromptsChange}
+            required={true}
           />
         </div>
 