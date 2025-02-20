991041efa89f73affd4be86f1d21ddff873d52c2
Rick Staa <rick.staa@outlook.com>
2025-02-20 10:03:02 +0100

    refactor(dev): improve docker file structure and docs
    This commit improves the docker file structure, adds a readme to explain
    the containers and adds a `--help` command.
    
    Co-authored-by: John | Elite Encoder <john@eliteencoder.net>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod +x /workspace/comfystream/.devcontainer/entrypoint.sh
+chmod +x /workspace/comfystream/docker/entrypoint.sh
 cd /workspace/comfystream
 
 # Install Comfystream in editable mode.
@@ -8,7 +8,7 @@ echo -e "\e[32mInstalling Comfystream in editable mode...\e[0m"
 /workspace/miniconda3/envs/comfystream/bin/python3 -m pip install -e . > /dev/null
 
 # Create a symlink to the entrypoint script.
-echo 'alias prepare_examples="/workspace/comfystream/.devcontainer/entrypoint.sh --download-models --build-engines"' >> ~/.bashrc
+echo 'alias prepare_examples="/workspace/comfystream/docker/entrypoint.sh --download-models --build-engines"' >> ~/.bashrc
 echo -e "\e[32mContainer ready! Run 'prepare_examples' to download models and build engines for example workflows.\e[0m"
 
 /bin/bash
