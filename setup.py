c9be7c39ee86bec5d1215e329677074543558056
John | Elite Encoder <john@eliteencoder.net>
2025-02-21 13:02:41 -0500

    bump comfystream version
    Update ComfyStream version to align with release tagging versioning convention

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="comfystream",
-    version="0.1.0",
+    version="0.0.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
