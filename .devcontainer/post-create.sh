b99a1c567e894dd934595d3200d4a2a0624b3b6e
John | Elite Encoder <john@eliteencoder.net>
2025-02-26 14:53:26 +0000

    compile static files for devcontainer using npm run build

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,8 +7,16 @@ cd /workspace/comfystream
 echo -e "\e[32mInstalling Comfystream in editable mode...\e[0m"
 /workspace/miniconda3/envs/comfystream/bin/python3 -m pip install -e . --root-user-action=ignore > /dev/null
 
+if [ ! -d "/workspace/comfystream/nodes/web/static" ]; then
+    echo -e "\e[32mBuilding web assets...\e[0m"
+    cd /workspace/comfystream/ui
+    npm install
+    npm run build
+fi
+
 # Create a symlink to the entrypoint script.
 echo 'alias prepare_examples="/workspace/comfystream/docker/entrypoint.sh --download-models --build-engines"' >> ~/.bashrc
 echo -e "\e[32mContainer ready! Run 'prepare_examples' to download models and build engines for example workflows.\e[0m"
 
+cd /workspace/comfystream
 /bin/bash
