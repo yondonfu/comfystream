71c75a055a32f788cd44deef099016098d6df97f
John | Elite Encoder <john@eliteencoder.net>
2025-03-07 06:05:40 +0000

    fix missing startup script

--- prestartup_script.py
+++ prestartup_script.py
@@ -1,18 +1,3 @@
-import os
-import sys
-import importlib.util
 
 
-#we use importlib as prestartup_script.py is not executed as part of the package
-# Get the current directory path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Path to the tensor_utils prestartup_script.py
-tensor_utils_script_path = os.path.join(current_dir, "nodes", "tensor_utils", "prestartup_script.py")
-
-# Load and execute the script directly
-spec = importlib.util.spec_from_file_location("tensor_utils_prestartup", tensor_utils_script_path)
-tensor_utils_module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(tensor_utils_module)
-
-print("Main prestartup_script.py loaded and executed tensor_utils version") 
+print("Main prestartup_script.py loaded") 
